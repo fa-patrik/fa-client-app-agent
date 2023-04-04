d0ef8f0bac8ebd52248b3a45aaa4914deab09de1
Patrik Wingqvist <patrik@fasolutions.com>
2023-04-04 16:26:06 +0200

    FACF-107. Buy and Sell modals now default to a tradeable portfolio.

--- src/components/TradingModals/useTradablePortfolioSelect.ts
+++ src/components/TradingModals/useTradablePortfolioSelect.ts
@@ -2,12 +2,24 @@ import { usePortfolioSelect } from "hooks/usePortfolioSelect";
 import { isPortfolioTradable } from "services/permissions/trade";
 
 export const useTradablePortfolioSelect = () => {
-  const { portfolioOptions, ...rest } = usePortfolioSelect();
+  const { portfolioOptions, setPortfolioId, portfolioId } =
+    usePortfolioSelect();
+  const tradeableOptions = portfolioOptions.filter(
+    (option) => option.details && isPortfolioTradable(option.details)
+  );
+
+  //it might be that the portfolioId from usePortfolioSelect is not tradeable
+  const portfolioIdIsTradeable = tradeableOptions.some(
+    (option) => portfolioId === option.id
+  );
+  //if it happens to be, use it, otherwise pick the first from tradeable options
+  const tradeablePortfolioId = portfolioIdIsTradeable
+    ? portfolioId
+    : tradeableOptions?.[0]?.id;
 
   return {
-    ...rest,
-    portfolioOptions: portfolioOptions.filter(
-      (option) => option.details && isPortfolioTradable(option.details)
-    ),
+    setPortfolioId,
+    portfolioId: tradeablePortfolioId,
+    portfolioOptions: tradeableOptions,
   };
 };
