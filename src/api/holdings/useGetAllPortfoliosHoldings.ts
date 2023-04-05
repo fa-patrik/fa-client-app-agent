2b9e1874022fe34456900118e4d2d61379b65af6
Patrik Wingqvist <patrik@fasolutions.com>
2023-04-05 11:05:45 +0200

    FACF-104. Contact and portfolio holdings with amount 0 are now filtered both in Holdings and Holding details views.

--- src/api/holdings/useGetAllPortfoliosHoldings.ts
+++ src/api/holdings/useGetAllPortfoliosHoldings.ts
@@ -3,7 +3,7 @@ import { useModifiedTranslation } from "hooks/useModifiedTranslation";
 import { useGetContractIdData } from "providers/ContractIdProvider";
 import { useGetContactInfo } from "../initial/useGetContactInfo";
 import { ALLOCATION_BY_SECURITY_TYPE_FIELDS } from "./fragments";
-import { AllPortfoliosHoldingsQuery } from "./types";
+import { AllPortfoliosHoldingsQuery, AllocationByType } from "./types";
 
 // to distinct Contact analytics from Portfolio analytics in Contact version we set portfolio.id as portfolio.parentPortfolioId (line 32)
 const HOLDINGS_QUERY = gql`
@@ -58,11 +58,37 @@ export const useGetAllPortfoliosHoldings = () => {
     loading,
     error,
     data: data && {
-      allocationByType:
-        data.contact.analytics.allocationTopLevel.allocationByType,
+      allocationByType: filterZeroPositions(
+        data?.contact?.analytics?.allocationTopLevel?.allocationByType
+      ),
       // there is no way to get currency for analytics under contact context,
       // but all portfolios have same currency, so we use currency from first one
       currency: portfoliosCurrency,
     },
   };
+};
+
+/**
+ * Removes positions with amount 0 (are returned by analytics if position was closed/sold today).
+ * @param allocationByType positions grouped by security type.
+ * @returns allocationByType but with positions with amount !== 0.
+ */
+export const filterZeroPositions = (allocationByType: AllocationByType[]) => {
+  return (
+    allocationByType?.reduce((prev, curr) => {
+      const filteredPositions = curr?.allocationsBySecurity?.filter(
+        (securityEntry) => {
+          return securityEntry?.figures?.amount !== 0;
+        }
+      );
+      //do not include type/category if no positions
+      if (filteredPositions?.length > 0) {
+        prev.push({
+          ...curr,
+          allocationsBySecurity: filteredPositions,
+        });
+      }
+      return prev;
+    }, [] as AllocationByType[]) || []
+  );
 };
