afe0e72546c51fe00ef8f0a4878f0112dbac72e8
Eugene Burdin <jevgeni.burdin@fasolutions.com>
2023-05-02 06:55:32 +0000

    Merged in FACF-108-hide-prospectus-and-kiid-icons- (pull request #6)
    FACF-108 hide prospectus and kiid icons
    
    * removed escape key to satisfy eslint warning
    
    * FACF-108 do full URL validity check
    
    * FAC-108 add another layer of checks based on url protocol.
    
    
    Approved-by: Jan Welti

--- src/components/DownloadableDocument/DownloadableDocument.tsx
+++ src/components/DownloadableDocument/DownloadableDocument.tsx
@@ -1,5 +1,6 @@
-import { HTMLAttributes } from "react";
+import { HTMLAttributes, useMemo } from "react";
 import { ReactComponent as FileIcon } from "assets/document-text.svg";
+import { isValidUrl } from "utils/url";
 
 interface DownloadableDocumentProps extends HTMLAttributes<HTMLAnchorElement> {
   label: string;
@@ -10,11 +11,21 @@ export const DownloadableDocument = ({
   label,
   url,
   ...anchorAttributes
-}: DownloadableDocumentProps) => (
-  <a target="_blank" rel="noopener noreferrer" {...anchorAttributes} href={url}>
-    <div className="flex justify-between text-primary-600 stroke-primary-600">
-      <div className="text-base font-semibold">{label}</div>
-      <FileIcon className="w-6 h-6 stroke-primary-600" />
-    </div>
-  </a>
-);
+}: DownloadableDocumentProps) => {
+  
+  const isValidURL: boolean = useMemo(() => isValidUrl(url), [url]);
+
+  return isValidURL ? (
+    <a
+      target="_blank"
+      rel="noopener noreferrer"
+      {...anchorAttributes}
+      href={url}
+    >
+      <div className="flex justify-between text-primary-600 stroke-primary-600">
+        <div className="text-base font-semibold">{label}</div>
+        <FileIcon className="w-6 h-6 stroke-primary-600" />
+      </div>
+    </a>
+  ) : null;
+};
--- src/utils/url.ts
+++ src/utils/url.ts
@@ -1,2 +1,5 @@
 export const addProtocolToUrl = (url: string) =>
   url.includes("https://") || url.includes("http://") ? url : "https://" + url;
+
+export const isValidUrl = (url: string) =>
+  /([a-z]+):\/\/(([a-z\d]([a-z\d-]*[a-z\d])*\.)+[a-z]{2,}|((\d{1,3}\.){3}\d{1,3}))(\/[-a-z\d%_.~+]*)*(\?[^\s]*)?(#[-a-z\d_]*)?/i.test(url);
