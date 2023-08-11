653268b627c2d9824a457bdca29b3747c1c4842f
Patrik Wingqvist <patrik@fasolutions.com>
2023-08-11 11:33:29 +0200

    Replaced hardcoded strings.

--- src/hooks/useExcelDownloader.ts
+++ src/hooks/useExcelDownloader.ts
@@ -2,13 +2,16 @@
 import { useCallback, useState } from 'react';
 import { Workbook } from 'exceljs';        
 import { saveAs } from "file-saver";
-
+const DEFAULT_FILE_NAME = "export.xlsx"
+const DEFAULT_SHEET_NAME = "Sheet 1"
+//https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types/Common_types
+const XLSX_MIME_TYPE = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
 const useExcelDownloader = (fileName?: string, sheetName?: string, rowsToBold?: number[]) => {
     const [loading, setLoading] = useState(false)
     const downloadExcel = useCallback(async (headers: string[], rows: unknown[][]) => {
         setLoading(true)
         const workbook = new Workbook();
-        const worksheet = workbook.addWorksheet(sheetName ?? 'Sheet 1');
+        const worksheet = workbook.addWorksheet(sheetName ?? DEFAULT_SHEET_NAME);
 
         worksheet.columns = headers.map(header => ({header, key: header}));
         rows.forEach(row => {
@@ -22,8 +25,8 @@ const useExcelDownloader = (fileName?: string, sheetName?: string, rowsToBold?: 
             worksheet.getRow(1).eachCell(c => c.style = {font: {bold: true}})
         }
         const buffer = await workbook.xlsx.writeBuffer();
-        const blob = new Blob([buffer], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
-        saveAs(blob,fileName ?? "export.xlsx")
+        const blob = new Blob([buffer], { type: XLSX_MIME_TYPE });
+        saveAs(blob,fileName ?? DEFAULT_FILE_NAME)
         setLoading(false)
     }, [fileName, sheetName,rowsToBold]);
 
