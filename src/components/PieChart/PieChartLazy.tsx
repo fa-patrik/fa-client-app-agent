df429eaca1a2622e5e2f2ada61a9e4d7140dca21
Patrik Wingqvist <patrik@fasolutions.com>
2024-04-17 16:00:04 +0200

    Added a config file and a hook to get the configs. Added a new lazy loading pie chart component that groups according the config file settings. Replaced the pie chart in portfolio overview with the new lazy loading pie chart.

---
+++ src/components/PieChart/PieChartLazy.tsx
@@ -0,0 +1,173 @@
+import { useMemo } from "react";
+import { gql, useQuery } from "@apollo/client";
+import { ApexOptions } from "apexcharts";
+import { AnalyticsGroupBy } from "api/types";
+import { getFetchPolicyOptions } from "api/utils";
+import { QueryLoadingWrapper } from "components/QueryLoadingWrapper/QueryLoadingWrapper";
+import { useModifiedTranslation } from "hooks/useModifiedTranslation";
+import { PieChart } from "./PieChart";
+
+const generateSeriesAndLabels = (groupData: Group[] | undefined) => {
+  return {
+    series: groupData?.map((data) =>
+      data?.firstAnalysis?.shareOfTotal !== undefined
+        ? data?.firstAnalysis?.shareOfTotal * 100
+        : undefined
+    ),
+    labels: groupData?.map((data) => data.name),
+  };
+};
+
+interface Group {
+  code: string;
+  name: string;
+  firstAnalysis: {
+    marketValue: number;
+    tradeAmount: number;
+    shareOfTotal: number;
+  };
+}
+
+interface AllocationsQuery {
+  analytics: {
+    grouppedAnalytics: {
+      portfolio: {
+        id: number;
+      };
+      firstAnalysis: {
+        marketValue: number;
+        tradeAmount: number;
+      };
+      group: Array<Group & { subGroup: Group[] }>;
+    };
+  };
+}
+
+const PERIOD_IN_DAYS = 1;
+const ALLOCATIONS_QUERY = gql`
+  fragment SubGroup on GrouppedAnalyticsDTO {
+    group: grouppedAnalytics {
+      code
+      name
+      firstAnalysis {
+        marketValue
+        tradeAmount
+        shareOfTotal
+      }
+    }
+  }
+  fragment Group on GrouppedAnalyticsDTO {
+    group: grouppedAnalytics {
+      code
+      name
+      firstAnalysis {
+        marketValue
+        tradeAmount
+        shareOfTotal
+      }
+      ...SubGroup
+    }
+  }
+  query GetAllocations(
+    $portfolioIds: [Long]
+    $locale: Locale
+    $startDate: String
+    $endDate: String
+    $groupBy: [GroupBy]
+    $groupCode: String
+  ) {
+    analytics(
+      parameters: {
+        pfIds: $portfolioIds
+        startDate: $startDate
+        endDate: $endDate
+        paramsSet: {
+          key: "allocations"
+          timePeriodCodes: "GIVEN"
+          grouppedByProperties: $groupBy
+          groupCode: $groupCode
+          includeData: false
+          includeChildren: true
+          drilldownEnabled: false
+          limit: 0
+          locale: $locale
+        }
+        includeDrilldownPositions: false
+      }
+    ) {
+      grouppedAnalytics(key: "allocations") {
+        portfolio {
+          id
+        }
+        firstAnalysis {
+          marketValue
+          tradeAmount
+        }
+        ...Group
+      }
+    }
+  }
+`;
+
+interface PieChartProps {
+  groupBy: AnalyticsGroupBy[] | undefined;
+  groupCode: string | undefined;
+  options?: ApexOptions;
+  portfolioIds: number[] | undefined;
+}
+
+const PieChartLazy = ({
+  groupBy = [AnalyticsGroupBy.TYPE],
+  groupCode,
+  options,
+  portfolioIds,
+}: PieChartProps) => {
+  const { i18n } = useModifiedTranslation();
+  const startDate = new Date();
+  startDate.setDate(startDate.getDate() - PERIOD_IN_DAYS);
+  const endDate = new Date();
+
+  const { loading, error, data } = useQuery<AllocationsQuery>(
+    ALLOCATIONS_QUERY,
+    {
+      variables: {
+        portfolioIds,
+        locale: i18n.language,
+        startDate: startDate.toLocaleDateString("sv-SE"),
+        endDate: endDate.toLocaleDateString("sv-SE"),
+        groupCode: groupCode,
+        groupBy: groupBy,
+      },
+      ...getFetchPolicyOptions(`GetAllocations.${portfolioIds}`),
+    }
+  );
+
+  const Pie = ({ data }: { data: AllocationsQuery | undefined }) => {
+    const { series: chartSeries, labels: chartLabels } = useMemo(() => {
+      const hasSubGroup =
+        !!data?.analytics.grouppedAnalytics.group[0]?.subGroup?.length;
+      return generateSeriesAndLabels(
+        hasSubGroup
+          ? data?.analytics.grouppedAnalytics.group[0]?.subGroup
+          : data?.analytics.grouppedAnalytics.group
+      );
+    }, [data]);
+
+    return (
+      <PieChart series={chartSeries} labels={chartLabels} options={options} />
+    );
+  };
+
+  return (
+    <>
+      <QueryLoadingWrapper
+        data={data}
+        loading={loading}
+        error={error}
+        SuccessComponent={Pie}
+      />
+    </>
+  );
+};
+
+export default PieChartLazy;
---
+++ src/hooks/useConfig.ts
@@ -0,0 +1,36 @@
+import { useState, useEffect } from "react";
+import { AnalyticsGroupBy } from "api/types";
+
+interface Config {
+  pages:
+    | {
+        portfolio:
+          | {
+              overview:
+                | {
+                    piechart:
+                      | {
+                          groupBy: AnalyticsGroupBy[] | undefined;
+                          groupCode: string | undefined;
+                        }
+                      | undefined;
+                  }
+                | undefined;
+            }
+          | undefined;
+      }
+    | undefined
+    | undefined;
+}
+
+export const useConfig = () => {
+  const [config, setConfig] = useState<Config | null>(null);
+
+  useEffect(() => {
+    fetch("/config/config.json")
+      .then((response) => response.json())
+      .then((json) => setConfig(json));
+  }, []);
+
+  return config;
+};
