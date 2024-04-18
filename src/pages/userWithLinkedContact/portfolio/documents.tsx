bccc203beb7295c9d9da0e54f76a427827aad665
Patrik Wingqvist <patrik@fasolutions.com>
2024-04-18 10:09:42 +0000

    Merged in feature/FACF-181 (pull request #46)
    Feature/FACF-181
    
    * Now fetches the portfolio's + its sub portfolios' documents + contact documents, if a portfolio is selected.
    
    * Changed the documents query logic to fetch either portfolio specific or all.
    
    * Merged develop into feature/FACF-181
    
    
    Approved-by: Jan Welti

--- src/pages/userWithLinkedContact/portfolio/documents.tsx
+++ src/pages/userWithLinkedContact/portfolio/documents.tsx
@@ -1,8 +1,25 @@
+import { useGetContactInfo } from "api/common/useGetContactInfo";
 import { useGetDocuments } from "api/documents/useGetDocuments";
 import { QueryLoadingWrapper } from "components";
+import { useGetContractIdData } from "providers/ContractIdProvider";
+import { useKeycloak } from "providers/KeycloakProvider";
+import { useParams } from "react-router-dom";
 import { Documents } from "views/documents/documents";
 
 export const DocumentsPage = () => {
-  const queryData = useGetDocuments();
+  const { linkedContact } = useKeycloak();
+  const { selectedContactId } = useGetContractIdData();
+  const { portfolioId } = useParams();
+  const portfolioIdAsNumber = portfolioId ? Number(portfolioId) : undefined;
+  const { data } = useGetContactInfo(false, selectedContactId ?? linkedContact);
+  const numberOfPortfolios = data?.portfolios?.length ?? 0;
+  const queryData = useGetDocuments(
+    //we get the portfolio specific docs if there is more than one portfolio
+    //and the user has selected a specific portfolio
+    //otherwise we get all documents, because the user cannot select 'total investments' in the dropdown
+    portfolioIdAsNumber !== undefined && numberOfPortfolios !== 1
+      ? portfolioIdAsNumber
+      : undefined
+  );
   return <QueryLoadingWrapper {...queryData} SuccessComponent={Documents} />;
 };
