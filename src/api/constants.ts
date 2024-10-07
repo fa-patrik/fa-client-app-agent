21e2453286b868f4dbfb855a5cb4a7d37df73694
Patrik Wingqvist <patrik@fasolutions.com>
2024-10-07 22:51:20 +0200

    FACF-216. Added possibility to set (sanitized) html into the banner. Added new dependency dompurify to sanitize html.

--- src/api/constants.ts
+++ src/api/constants.ts
@@ -1,1 +1,16 @@
 export const ADVISOR_TAG = "apitradeorderAdvisor";
+/** Allowed attributes for dangerously inset html */
+export const ALLOWED_HTML_ATTRIBUTES = [
+  "href",
+  "target",
+  "rel",
+  "style",
+  "id",
+  "class",
+  "src",
+  "alt",
+  "title",
+  "width",
+  "height",
+  "data-*",
+];
